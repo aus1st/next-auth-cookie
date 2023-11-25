22e27759a4019817ac75db8c3a563ee0f9be4f27
Ahmed Siddiqui <aus1st@gmail.com>
2023-11-25 18:44:31 +0500

    firstCommit

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,38 @@
+import { NextRequest, NextResponse } from "next/server";
+import jwt from 'jsonwebtoken'
+
+export async function POST(request: NextRequest) {
+    const { uid } = await request.json()
+
+    if (uid) {
+        const usr = {
+            AD_USRID: 'siddiqui.ahmed',
+            AD_USRDEPT: 'IT',
+            AD_USRNAME: 'Ahmed Siddiqui',
+            API_TOKEN: '123456789',
+        }
+        const token = jwt.sign(
+            usr,
+            process.env.JWT_SECRET as string,
+            { expiresIn: '2m' }
+        )
+
+        const response = NextResponse.json({
+            message: 'success',
+            success: true,
+            user: usr
+        });
+
+        response.cookies.set('token', token, {
+            httpOnly: true,
+            maxAge: 900
+        });
+
+        return response;
+
+
+    }
+
+
+
+}
